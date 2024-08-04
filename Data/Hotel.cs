a0880ad4fcb5713b8f6a2993c72f84bb975b1944
Tina <hnewbu1@gmail.com>
2024-08-03 22:50:19 -0400

    added entity framework ad created database.

---
+++ Data/Hotel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListing.Data
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating {  get; set; }
+
+        //Foreign Key....to create a new DB Table
+        //Ctrl + . on the name and select generate new type
+        //and then slide for more to get new type in a new file
+
+
+        [ForeignKey(nameof(CountryId))]
+        public int CountryId { get; set; }
+
+        public Country Country { get; set; }
+    }
+}
---
+++ Data/HotelListingDbContext.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Data
+{
+    public class HotelListingDbContext: DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        { 
+        
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1, 
+                    Name = "United States",
+                    ShortName = "US"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "United Kingdom",
+                    ShortName = "UK"
+
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Bahamas",
+                    ShortName = "BS"
+                }
+            );
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Negril",
+                    CountryId = 3,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "The Comfort Inn",
+                    Address = "California",
+                    CountryId = 1,
+                    Rating = 3.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Harrods Hotel",
+                    Address = "London",
+                    CountryId = 2,
+                    Rating = 4.9
+                }
+            );
+        }
+    }
+}
