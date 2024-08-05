e6dc3840409fc14ff1da9bfe02bc47bb5341c28f
Tina <hnewbu1@gmail.com>
2024-08-04 21:18:56 -0400

    added notes

---
+++ Controllers/CountriesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelListing.Data;
+using System.Runtime.Intrinsics.X86;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        //DB (Dependency)? Injection (Video 26 with Williams)
+        //(registered DBContext as a service in the program.cs.
+        //Now can inject dbcontext in file you want,
+        //example: need to interact with db in the controller
+        //1) have a constructor
+        //2) call in the parameter of the datatype and give it a name
+        //3) initialize a private field that should be equal to what is being passed in the constructor
+        //  We don't have to declare a new instance of the DB context whenever we have a new class
+        //  we can simply inject, the I in SOLID, Inversion of Control
+        // we don't have to instiatate a new db context every time, and helps the with the lifetime of the context
+        // all being handled by the entire program
+        //as soon as a request comes in, and we do whatever do in the db.  When the request is over, it kills the db instance in the background.
+        //we don't have to be in charge of it, and it saves memory, time, far more efficient.   
+
+
+        //Task - A task in C# is used to implement Task-based Asynchronous Programming. The Task object is typically executed asynchronously on a thread pool thread rather than synchronously on the main thread of the application.
+        //C# supports parallel execution of code through multithreading. A thread is an independent execution path, able to run simultaneously with other threads.
+        //The main thread creates a new thread t on which it runs a method that repeatedly prints the character “y”. Simultaneously, the main thread repeatedly prints the character “x”:
+        //https://www.albahari.com/threading/
+
+        //async - Signals to the compiler that this method contains an await statement; it contains asynchronous operations.
+
+        //await - The await keyword provides a non-blocking way to start a task, then continue execution when that task completes.
+
+        //ActionResult - An action is capable of returning a specific data type (see WeatherForecastController action).  When multiple return types are possible, it's common to return ActionResult, IActionResult or ActionResult<T>, where T represents the data type to be returned.
+
+        private readonly HotelListingDbContext _context;
+
+        //constructor
+        public CountriesController(HotelListingDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Countries
+        [HttpGet]
+
+        //IEnumberable - return at least one or more in a collection
+        public async Task<ActionResult<IEnumerable<Country>>> GetCountries()
+        {
+            //return Select * from Countries as a List
+            //OK can be used to return 200 explictly
+            var countries = await _context.Countries.ToListAsync();
+            return Ok(countries);
+
+        }
+
+        // GET: api/Countries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Country>> GetCountry(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(country);
+        }
+
+        // PUT: api/Countries/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        //All or nothing was changed
+        //Country is the entire object being sent from the UI/Client
+        public async Task<IActionResult> PutCountry(int id, Country country)
+        {
+            if (id != country.Id)
+            {
+                return BadRequest("Invalid Record Id");
+            }
+
+            //every entity (ex: country) has an Entity State
+
+            _context.Entry(country).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //If two users try to update the same record
+                if (!CountryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //This methods expects to return a datatype of Country
+        // POST: api/Countries
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Country>> PostCountry(Country country)
+        {
+            //context represents
+            //a copy of what is being passwed in controller's constructor, DBContext
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+
+            // this is what is returned in Swagger from the return in code:
+            // access-control-allow-origin: *
+            // content-type: application/json;
+            // charset=utf-8  date: Sun,04 Aug 2024 22:20:23 GMT  
+            //location: http://localhost:5292/api/Countries/4
+            //server: Kestrel  transfer-encoding: chunked 
+
+            return CreatedAtAction("GetCountry", new { id = country.Id }, country);
+
+        }
+
+        // DELETE: api/Countries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountry(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            //Entity State is in a delete status, so generate a delete SQL?
+            //Entity Framework statement
+
+            _context.Countries.Remove(country);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CountryExists(int id)
+        {
+            return _context.Countries.Any(e => e.Id == id);
+        }
+    }
+}
